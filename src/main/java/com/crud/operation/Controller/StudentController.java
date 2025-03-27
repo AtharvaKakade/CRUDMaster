4fa736302f4eefae51c3c6121fd9a824e75e7b95
AtharvaKakade <kakadeatharva5@gmail.com>
2025-03-27 18:37:44 +0530

    Jwt update

--- src/main/java/com/crud/operation/Controller/StudentController.java
+++ src/main/java/com/crud/operation/Controller/StudentController.java
@@ -2,14 +2,27 @@ package com.crud.operation.Controller;
 
 import com.crud.operation.Entity.Student;
 import com.crud.operation.Service.StudentService;
+import com.crud.operation.jwt.LoginRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.crud.operation.jwt.*;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/students")
@@ -17,17 +30,53 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException exception) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        LoginResponse response = new LoginResponse(userDetails.getUsername(), roles, jwtToken);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
     public Student createStudent(@Valid @RequestBody Student student) {
         return studentService.createStudent(student);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PutMapping("/{id}")
     public ResponseEntity<Student> updateStudent(
             @PathVariable Long id,
@@ -39,6 +88,7 @@ public class StudentController {
     }
 
     // Delete student
+    @PreAuthorize("hasRole('USER')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStudent(@Valid @PathVariable Long id) {
         studentService.deleteStudent(id);
--- src/main/java/com/crud/operation/OperationApplication.java
+++ src/main/java/com/crud/operation/OperationApplication.java
@@ -8,6 +8,7 @@ public class OperationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OperationApplication.class, args);
+
 	}
 
 }
